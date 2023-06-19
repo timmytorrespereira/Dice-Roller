b84d0eaf5828cce609eeba5d0c0c954e72b4c866
timmytorrespereira <timmy.ryder@icloud.com>
2023-06-19 20:06:44 +0200

    Testing for Model

--- ModelTest.java
+++ ModelTest.java
@@ -1,16 +1,25 @@
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
 public class ModelTest {
-    public static void main(String[] args){
-        randomRollShouldBeBetween1andSize();
+    private Model model;
+    private View view;
+
+    @BeforeEach
+    public void setup(){
+        model = new Model();
+        view = new View();
+
     }
-    
-    public static void randomRollShouldBeBetween1andSize(){
-        Model model = new Model();
+
+    @Test
+    public void randomRollShouldBeBetween1andSize(){
         model.generateRandomRoll(3);
         int randroll = model.getRandRoll();
         Assertions.assertTrue(randroll>=1 && randroll<=3, "Random number should be between 1 and 3");
     }
+
 }
